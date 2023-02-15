9109156b36012589d01d8c2daeaf83673ed32391
Marc <meir.guggenheim@gmail.com>
2023-02-15 18:24:50 -0500

    post a review

--- frontend/src/components/CreateReviewModal/index.js
+++ frontend/src/components/CreateReviewModal/index.js
@@ -7,6 +7,8 @@ import { getAllSpots, getSingleSpot } from '../../store/spots';
 import { useModal } from '../../context/Modal';
 import { postReview } from '../../store/reviews';
 
+import './Review.css'
+
 export default function CreateReviewModal({spotId}) {
     const [review, setReview] = useState('')
     const [stars, setStars] = useState(1)
@@ -32,6 +34,18 @@ export default function CreateReviewModal({spotId}) {
             </div>
             <div>
                 Stars
+                <div class="rate">
+                  <input type="radio" id="star5" name="rate" value="5" />
+                  <label for="star5" title="text">5 stars</label>
+                  <input type="radio" id="star4" name="rate" value="4" />
+                  <label for="star4" title="text">4 stars</label>
+                  <input type="radio" id="star3" name="rate" value="3" />
+                  <label for="star3" title="text">3 stars</label>
+                  <input type="radio" id="star2" name="rate" value="2" />
+                  <label for="star2" title="text">2 stars</label>
+                  <input type="radio" id="star1" name="rate" value="1" />
+                  <label for="star1" title="text">1 star</label>
+                </div>
             </div>
             <div>
                 <button type='submit'>Submit Your Review</button>
