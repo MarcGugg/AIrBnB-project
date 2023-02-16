3e339e82217574dd8f45ebfb987cc96b1679206b
Marc <meir.guggenheim@gmail.com>
2023-02-16 17:43:41 -0500

    css

--- frontend/src/components/SingleSpot/index.js
+++ frontend/src/components/SingleSpot/index.js
@@ -49,44 +49,36 @@ export default function SingleSpot() {
     return (
         <>
         <div>
-            <header className='spotNameParent'>             
+            <div className='nameLocationParent'>
             <div className='spotName'>
             {spot.name}
             </div>
-            </header>
             <div className='spotLocation'>
                 {spot.city}, {spot.state}, {spot.country}
             </div>
+            </div>
+            <div className='imageParent'>                
             <div className='image'>
             <img src={spot.SpotImages[0].url} style={{width: 700, height: 500}}/>
             </div>
+            </div>
+            <div className='hostDescCallout'>
+            <div className='hostAndDesc'>                
             <div className='spotOwner'>
+                <h1>
             hosted by: {spot.Owner.firstName} {spot.Owner.lastName}
+                </h1>
             </div>
             <div className='spotDescription'>
+                <h4>
             {spot.description}
+                </h4>
             </div>
-            <div>
+            {/* <div className='reviewButton'> */}
                 {/* put modal component into ternary */}
-                {user && spot.Owner.id !== user.id ? <OpenModalButton modalComponent={<CreateReviewModal spotId={spotId} user={user}/>} buttonText={'Post Your Review'} />: ''} 
-            </div>
-            <div>
-                {reviews && Object.values(reviews).map(review => <li>
-                    <div>
-                        {review.User.firstName} {review.User.lastName}
-                    </div>
-                    <div>
-                    {review.review}
-                    </div>
-                    <div>
-                        {user && review.userId === user.id ? <OpenModalButton modalComponent={<UpdateReviewModal reviewId={review.id} user={user}/>} buttonText={'Edit Your Review'}/>: ''}
-                    </div>
-                    <div>
-                        {user && review.userId === user.id ? <button onClick={() => handleClick(review.id)}>Delete Review</button>: ''}
-                    </div>
-                </li>)}
-            </div>
-        </div>
+                {/* {user && spot.Owner.id !== user.id ? <OpenModalButton modalComponent={<CreateReviewModal spotId={spotId} user={user}/>} buttonText={'Post Your Review'} />: ''}  */}
+            {/* </div> */}
+            </div>           
         <div className='calloutInfoBoxParent'>
         <div className='calloutInfoBox'>
             <div className='priceRatingParent'>
@@ -102,6 +94,29 @@ export default function SingleSpot() {
             </div>
         </div>
         </div>
+
+            </div>
+        </div>
+        <div className='reviewButton'>
+                {/* put modal component into ternary */}
+                {user && spot.Owner.id !== user.id ? <OpenModalButton modalComponent={<CreateReviewModal spotId={spotId} user={user}/>} buttonText={'Post Your Review'} />: ''} 
+            </div>
+            <div className='reviews'>
+                {reviews && Object.values(reviews).map(review => <li>
+                    <div>
+                        {review.User.firstName} {review.User.lastName}
+                    </div>
+                    <div>
+                    {review.review}
+                    </div>
+                    <div>
+                        {user && review.userId === user.id ? <OpenModalButton modalComponent={<UpdateReviewModal reviewId={review.id} user={user}/>} buttonText={'Edit Your Review'}/>: ''}
+                    </div>
+                    <div>
+                        {user && review.userId === user.id ? <button onClick={() => handleClick(review.id)}>Delete Review</button>: ''}
+                    </div>
+                </li>)}
+            </div>
         </>
     );
 }
