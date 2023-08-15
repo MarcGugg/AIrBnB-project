f5afe3a011feee7a27a7c539163a8a969d06c87a
Marc <meir.guggenheim@gmail.com>
2023-08-15 13:34:40 -0400

    display user bookings

--- frontend/src/components/ManageUserBookings/index.js
+++ frontend/src/components/ManageUserBookings/index.js
@@ -3,6 +3,8 @@ import { getUserBookings } from "../../store/bookings"
 import { useEffect, useState } from "react"
 import { useSelector, useDispatch } from "react-redux"
 
+import './userBookings.css'
+
 export default function UserBookings() {
     const dispatch = useDispatch()
     // const {userId} = useParams()
@@ -19,7 +21,21 @@ export default function UserBookings() {
 
     return (
         <>
-        <h1>User Bookings</h1>
+        <h1>Your Bookings</h1>
+        {bookings && Object.values(bookings).length > 0 ? 
+        Object.values(bookings).map(booking => 
+        <div className="wholeBooking">
+            <img src={booking.Spot.previewImage} className="preview"/>
+            <div className="bookingInfo">
+            <h2>{booking.Spot?.name}</h2>
+            <p>Start Date: {booking.startDate}</p>
+            <p>End Date: {booking.endDate}</p>
+            <p>Booked On: {booking.createdAt.slice(0, 10)}</p>
+            <h3>Price /Night: ${booking.Spot.price}</h3>
+            </div>
+        </div>    
+        )
+        : <h1>You have not made any bookings</h1>}
         </>
     )
 }
